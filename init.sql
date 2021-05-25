ef6b1fc582410e08c0c4ecf782cdd3f54643ef75
Nikita <nik180101@gmail.com>
2021-05-25 16:59:14 +0700

    docker

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE movinf OWNER postgres;
 CREATE USER guest WITH PASSWORD '123';
+GRANT ALL PRIVILEGES ON movinf TO guest;
 \connect movinf;
-CREATE SCHEMA "movinf-schema" AUTHORIZATION guest;
+CREATE SCHEMA "movinf-schema";
